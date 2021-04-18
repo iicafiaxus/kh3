604a4b850460d04f859cec1d3691ef8bdbf39d61
iicafiaxus <iica@live.jp>
2021-04-18 13:49:35 +0900

    セーフモード画面を追加

--- kh3ui.js
+++ kh3ui.js
@@ -336,19 +336,6 @@ kh3ui.fileNew2 = function(title){
 }
 
 // ------------------------------
-// ユーティリティ：Base64
-// ------------------------------
-// https://developer.mozilla.org/en-US/docs/Web/API/WindowOrWorkerGlobalScope/btoa
-// ucs-2 string to base64 encoded ascii
-function utoa(str) {
-    return window.btoa(unescape(encodeURIComponent(str)));
-}
-// base64 encoded ascii to ucs-2 string
-function atou(str) {
-    return decodeURIComponent(escape(window.atob(str)));
-}
-
-// ------------------------------
 // インポートしたテキストを反映
 // ------------------------------
 kh3ui.applyImportedText = function(text){
--- kh3uifile.js
+++ kh3uifile.js
@@ -46,3 +46,17 @@ kh3ui.file.reload = function(){
 
 
 */
+
+// ------------------------------
+// ユーティリティ：Base64
+// ------------------------------
+// https://developer.mozilla.org/en-US/docs/Web/API/WindowOrWorkerGlobalScope/btoa
+// ucs-2 string to base64 encoded ascii
+function utoa(str) {
+    return window.btoa(unescape(encodeURIComponent(str)));
+}
+// base64 encoded ascii to ucs-2 string
+function atou(str) {
+    return decodeURIComponent(escape(window.atob(str)));
+}
+
