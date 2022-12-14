c565b3cc7caaf87636abc5cffd21f9daf5fa9058
iicafiaxus <iica@live.jp>
2022-12-15 00:34:19 +0900

    subsupに対応

--- kh3indexed.js
+++ kh3indexed.js
@@ -4,8 +4,9 @@ kh3.Indexed = function(pos = "sub"){
 	this.pos = pos;
 	this.canRotate = 1;
 	this.mainunits = [];
-	this.indexunits = [];
-	this.turn = 1; // 1=main 2=index
+	this.subunits = [];
+	this.supunits = [];
+	this.turn = 1; // 1=main 2=sub 3=sup
 }
 kh3.Indexed.prototype = Object.create(kh3.Unit.prototype);
 kh3.Indexed.prototype.constractor = kh3.Indexed;
@@ -22,21 +23,28 @@ kh3.Indexed.prototype.makeDom = function(){
 	this.span = document.createElement("span");
 	for(unit of this.mainunits) unit.makeDom();
 	for(unit of this.mainunits) this.span.appendChild(unit.span);
-	for(unit of this.indexunits){
-		unit.pos = this.pos;
+	for(unit of this.subunits){
+		unit.pos = "sub";
 		unit.makeDom();
 	}
-	for(unit of this.indexunits) this.span.appendChild(unit.span);
+	for(unit of this.supunits){
+		unit.pos = "sup";
+		unit.makeDom();
+	}
+	for(unit of this.subunits) this.span.appendChild(unit.span);
+	for(unit of this.supunits) this.span.appendChild(unit.span);
 
 	this.setSubpositions();
 }
 
 kh3.Indexed.prototype.turnover = function(){
-	this.turn = 2;
+	if(this.turn == 1 && this.pos == "sup")	this.turn = 3;
+	else if(this.turn == 1)	this.turn = 2;
+	else this.turn = 3;
 }
 
 kh3.Indexed.prototype.close = function(){
-	if(this.turn == 1) this.turnover();
+	if(this.turn == 1 || this.turn == 2 && this.pos == "subsup") this.turnover();
 	else this.isClosed = 1;
 }
 
@@ -48,7 +56,12 @@ kh3.Indexed.prototype.add = function(unit){
 		this.firstchar = this.mainunits[0].firstchar;
 		this.lastchar = unit.lastchar;
 	}
-	else this.indexunits.push(unit);
+	else if(this.turn == 2){
+		this.subunits.push(unit);
+	}
+	else{
+		this.supunits.push(unit);
+	}
 }
 
 kh3.Indexed.prototype.setSubpositions = function(){
@@ -60,25 +73,41 @@ kh3.Indexed.prototype.setSubpositions = function(){
 	for(unit of this.mainunits){
 		if(lastunit) left += lastunit.marginTo(unit);
 		unit.left = left;
-		unit.top = (this.pos == "sup"? kh3.setting.zh * 0.2: 0);
+		unit.top = ((this.pos == "sup" || this.pos == "subsup") ? kh3.setting.zh * 0.2 : 0);
 		this.innerwidth = (left += unit.width);
+		this.innerheight = Math.max(this.innerheight, unit.height);
+		this.innermiddle = Math.max(this.innermiddle, unit.middle);
+		lastunit = unit;
+	}
+	var indexwidth = 0;
+	var indexleft = left;
+	lastunit = null;
+	for(unit of this.subunits){
+		if(lastunit) left += lastunit.marginTo(unit) * 0.7 * 0.4;
+		unit.left = left;
+		unit.top = this.innerheight - kh3.setting.zh * (0.75 - (this.pos == "subsup"? 0.2: 0));
+		indexwidth = (left += unit.width);
 		this.innerheight = Math.max(this.innerheight, unit.height);
 		this.innermiddle = Math.max(this.innermiddle, unit.middle);
 		lastunit = unit;
 	}
 	lastunit = null;
-	for(unit of this.indexunits){
+	left = indexleft;
+	for(unit of this.supunits){
 		if(lastunit) left += lastunit.marginTo(unit) * 0.7 * 0.4;
 		unit.left = left;
-		unit.top = (this.pos == "sup"? kh3.setting.zh * -0.2: kh3.setting.zh * 0.25);
-		this.innerwidth = (left += unit.width);
+		unit.top = kh3.setting.zh * -0.2;
+		indexwidth = Math.max(indexwidth, left += unit.width);
 		this.innerheight = Math.max(this.innerheight, unit.height);
 		this.innermiddle = Math.max(this.innermiddle, unit.middle);
 		lastunit = unit;
 	}
-	this.width = this.innerwidth + kh3.setting.zw * (this.pos == "sup"? 0: 0.1);
-	this.height = this.innerheight + kh3.setting.zh * (this.pos == "sup"? 0.2: 0);
-	this.middle = this.innermiddle + kh3.setting.zh * (this.pos == "sup"? 0.2: 0);
+	this.innerwidth = indexwidth;
+	this.width = this.innerwidth + kh3.setting.zw * (this.pos == "sup"? 0: 0);
+	this.height = this.innerheight +
+		kh3.setting.zh * ((this.pos == "sup" || this.pos == "subsup")? 0.2: 0);
+	this.middle = this.innermiddle +
+		kh3.setting.zh * ((this.pos == "sup" || this.pos == "subsup")? 0.2: 0);
 
 	
 	// 縦中横の処理
@@ -88,7 +117,12 @@ kh3.Indexed.prototype.setSubpositions = function(){
 			unit.left = t;
 			unit.top = this.width / 2 - l -  kh3.setting.zh / 2;
 		}
-		for(unit of this.indexunits){
+		for(unit of this.subunits){
+			var l = unit.left, t = unit.top;
+			unit.left = t;
+			unit.top = this.width / 2 - l -  kh3.setting.zh / 2;
+		}
+		for(unit of this.supunits){
 			var l = unit.left, t = unit.top;
 			unit.left = t;
 			unit.top = this.width / 2 - l -  kh3.setting.zh / 2;
@@ -113,7 +147,10 @@ kh3.Indexed.prototype.rotate = function(){
 	for(u of this.mainunits){
 		u.span.className += " kh3-rotated";
 	}
-	for(u of this.indexunits){
+	for(u of this.subunits){
+		u.span.className += " kh3-rotated";
+	}
+	for(u of this.supunits){
 		u.span.className += " kh3-rotated";
 	}
 
@@ -135,7 +172,13 @@ kh3.Indexed.prototype.setPosition = function(){
 	}
 
 	// 添字を配置
-	for(u of this.indexunits){
+	for(u of this.subunits){
+		u.left += this.left, u.top += this.top;
+		this.innertop = Math.min(this.innertop, u.top);
+		this.innermiddle = Math.max(this.innermiddle, u.middle);
+		u.setPosition();
+	}
+	for(u of this.supunits){
 		u.left += this.left, u.top += this.top;
 		this.innertop = Math.min(this.innertop, u.top);
 		this.innermiddle = Math.max(this.innermiddle, u.middle);
